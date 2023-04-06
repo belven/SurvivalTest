542bc3104908e71467e15b95648fa4badfe3802a
Sam King <belven000@gmail.com>
2023-04-06 09:15:55 +0100

    Moved .git

---
+++ Source/SurvivalTest/Items/LootBox.h
@@ -0,0 +1,56 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ItemStructs.h"
+#include "SurvivalTest/Interactable.h"
+#include "SurvivalTest/ItemContainerInterface.h"
+#include "LootBox.generated.h"
+
+class UItemContainer;
+
+UCLASS()
+class SURVIVALTEST_API ALootBox : public AActor, public IInteractable, public IItemContainerInterface
+{
+	GENERATED_BODY()
+
+public:
+	ALootBox();
+	void SetUpBox();
+	virtual void Tick(float DeltaTime) override;
+	virtual void OnConstruction(const FTransform& Transform) override;
+	virtual UItemContainer* GetItemContainer() override { return container; }
+	virtual void Interact(ABasePlayerController* instigator) override;
+	virtual void Highlight(bool activate) override;
+	FInstanceItemData CreateLoot(FItemData id);
+
+protected:
+	virtual void BeginPlay() override;
+
+	UBaseGameInstance* GetGame();
+	void CreateLootboxData();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = LootBox, meta = (AllowPrivateAccess = "true"))
+		UStaticMesh* boxMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = LootBox, meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* boxMeshComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = LootBox, meta = (AllowPrivateAccess = "true"))
+		int32 boxID;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = LootBox, meta = (AllowPrivateAccess = "true"))
+	int32 containerID = 2;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = LootBox, meta = (AllowPrivateAccess = "true"))
+		int32 itemQuantity;
+
+	FInstanceContainerData icd;
+	FInstanceBoxData ibd;
+	bool isHighlighted = false;
+	
+	UPROPERTY()
+	UItemContainer* container;
+
+	UPROPERTY()
+	UBaseGameInstance* gameIn;
+};
---
+++ Source/SurvivalTestEditor.Target.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class SurvivalTestEditorTarget : TargetRules
+{
+	public SurvivalTestEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.Add("SurvivalTest");
+	}
+}
